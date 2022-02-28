e8a811e85dc27de979f9539873497ee8ea2b483a
robokae <alexanderhom19@gmail.com>
2022-02-27 20:52:54 -0800

    add timeline activity and tweet item layout

--- app/src/main/java/com/codepath/apps/restclienttemplate/RestClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/RestClient.java
@@ -6,6 +6,7 @@ import com.codepath.asynchttpclient.RequestParams;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.codepath.oauth.OAuthBaseClient;
 import com.github.scribejava.apis.FlickrApi;
+import com.github.scribejava.apis.TwitterApi;
 import com.github.scribejava.core.builder.api.BaseApi;
 
 /*
@@ -21,10 +22,10 @@ import com.github.scribejava.core.builder.api.BaseApi;
  * 
  */
 public class RestClient extends OAuthBaseClient {
-	public static final BaseApi REST_API_INSTANCE = FlickrApi.instance(FlickrApi.FlickrPerm.WRITE); // Change this
-	public static final String REST_URL = "https://api.flickr.com/services"; // Change this, base API URL
-	public static final String REST_CONSUMER_KEY = BuildConfig.CONSUMER_KEY;       // Change this inside apikey.properties
-	public static final String REST_CONSUMER_SECRET = BuildConfig.CONSUMER_SECRET; // Change this inside apikey.properties
+	public static final BaseApi REST_API_INSTANCE = TwitterApi.instance();
+	public static final String REST_URL = "https://api.twitter.com/1.1";
+	public static final String REST_CONSUMER_KEY = BuildConfig.CONSUMER_KEY;
+	public static final String REST_CONSUMER_SECRET = BuildConfig.CONSUMER_SECRET;
 
 	// Landing page to indicate the OAuth flow worked in case Chrome for Android 25+ blocks navigation back to the app.
 	public static final String FALLBACK_URL = "https://codepath.github.io/android-rest-client-template/success.html";
